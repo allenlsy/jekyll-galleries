c3f28bfafbfaa58c0815096241f33f9378afd461
allenlsy <cafe@allenlsy.com>
2014-02-18 08:02:02 +0800

    read config from yml

--- lib/jekyll/galleries.rb
+++ lib/jekyll/galleries.rb
@@ -1,7 +1,9 @@
 module Jekyll
   class GalleryGenerator < Generator
-    attr_accessor :site, :gallery_dir, :gallery_layout
+    attr_accessor :site, :gallery_dir, :gallery_layout, :galleries
     class << self; attr_accessor :site; end
+
+    CONFIG_GALLERIES_ATTR = 'galleries'
 
     def generate(site)
       self.class.site = self.site = site
@@ -10,6 +12,8 @@ module Jekyll
 
       # array of GalleryPage objects
       site.data['galleries'] = []
+
+      # site.galleries = []
       gallery_dirs = Dir["#{site.source}/#{gallery_dir}/*/"].select { |e| File.directory? e }
       gallery_dirs.reverse! # sort by date desc
       gallery_dirs.each do |dir|
@@ -22,6 +26,7 @@ module Jekyll
       data = { 'layout' => gallery_layout }
 
       page = GalleryPage.new(site, site.source, self.gallery_dir, gallery_dir, data)
+
       site.pages << page
 
       site.data['galleries'] << page
@@ -32,19 +37,40 @@ module Jekyll
   class GalleryPage < Page
     # Valid post name regex.
     MATCHER = /^(\d+-\d+-\d+)-(.*)$/
+    CONFIG_GALLERIES_ATTR = 'galleries'
 
-    attr_accessor :site, :url, :name, :slug
+    attr_accessor :url, :name, :slug, :date
 
     def initialize(site, base, gen_dir, dir, data={})
       self.content = data.delete('content') || ''
       self.data = data
 
-      super(site, base, gen_dir, File.basename(dir) )
+      dir_name = File.basename dir
+      super(site, base, gen_dir, dir_name)
+
+      # photo configuration
+      photos_config_filepath =  "#{base}/#{site.config['gallery_dir']}/#{self.data['name']}.yml"
+      photos_config = YAML.load(File.open(photos_config_filepath).read) if File.exists?(photos_config_filepath)
 
       # url, photos
       self.url = "/#{gen_dir}/#{self.data['slug']}.html"
       self.data['url'] = URI.escape self.url
-      self.data['photo_urls'] = Dir["#{base}/#{gen_dir}/#{self.name}/*"].map { |e| URI.escape("/#{gen_dir}/#{self.name}/#{File.basename e}") }
+      photos = Dir["#{base}/#{gen_dir}/#{dir_name}/*"].map { |e| { filename: File.basename(e), url: URI.escape("/#{gen_dir}/#{dir_name}/#{File.basename e}") } }
+      self.data['photos'] = []
+      photos.each do |photo|
+        photo_data = {}
+        photo_data = photos_config.find { |e| e['filename'] == photo[:filename] } if photos_config
+
+        self.data['photos'] << Photo.new(photo[:filename], photo[:url], photo_data)
+      end
+
+      # gallery page attributes
+      if site.config[CONFIG_GALLERIES_ATTR]
+        attr = site.config[CONFIG_GALLERIES_ATTR].find { |e| e['title'] == self.name}
+        if attr
+          attr.each { |k, v| self.data[k] = v }
+        end
+      end
     end
 
     def read_yaml(*)
@@ -59,11 +85,26 @@ module Jekyll
       m, date, name = *name.match(MATCHER)
 
       # self.data['date'] = Time.parse(date)
-      self.data['date'] = date
-      self.data['name'] = name
-      self.data['slug'] = name
+      self.data['date'] = self.date = date
+      self.data['name'] = self.name = name
+      self.data['slug'] = self.slug = name.gsub(/[^0-9a-z ]/i, '').downcase.gsub(/ /, '-')
     rescue ArgumentError
       raise FatalException.new("Gallery #{name} does not have a valid date.")
+    end
+  end
+
+  class Photo
+    attr_accessor :filename, :url, :data
+
+    def initialize(filename, url, options=nil)
+      self.filename = filename
+      self.url = url
+
+      self.data = options || {}
+    end
+
+    def to_liquid
+      self.data.merge({ 'url' => self.url })
     end
   end
 
